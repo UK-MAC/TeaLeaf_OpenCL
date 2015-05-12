96b00157fa49fd36880af80d33a3bd95f97ee60b
Michael Boulton <michael.boulton@bristol.ac.uk>
2015-05-12 14:21:54 +0100

    Made reduction able to do multiple reductions at once to reduce cynchronisaton

--- field_summary_kernel_ocl.cpp
+++ field_summary_kernel_ocl.cpp
@@ -13,9 +13,16 @@ void TeaCLContext::field_summary_kernel
     //ENQUEUE(field_summary_device);
     ENQUEUE_OFFSET(field_summary_device);
 
-    *vol = reduceValue<double>(sum_red_kernels_double, reduce_buf_1);
-    *mass = reduceValue<double>(sum_red_kernels_double, reduce_buf_2);
-    *ie = reduceValue<double>(sum_red_kernels_double, reduce_buf_3);
-    *temp = reduceValue<double>(sum_red_kernels_double, reduce_buf_4);
+    std::vector<int> indexes;
+    indexes.push_back(1);
+    indexes.push_back(2);
+    indexes.push_back(3);
+    indexes.push_back(4);
+    std::vector<double> reduced_values = sumReduceValues<double>(indexes);
+
+    *vol = reduced_values.at(0);
+    *mass = reduced_values.at(1);
+    *ie = reduced_values.at(2);
+    *temp = reduced_values.at(3);
 }
 
